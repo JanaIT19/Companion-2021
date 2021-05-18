3a3fad0c4f779db7845d7d2265e754089f4add13
Jana <jana.pr.art@gmail.com>
2021-05-18 21:05:27 +0300

    Animation, main menu buttons

---
+++ Assets/Scripts/RotatingObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingObject : MonoBehaviour
+{
+    public float Speed = 90f;
+
+    private float objectRotation = 0f;
+
+    void Update()
+    {
+        objectRotation += Time.deltaTime * Speed;
+        this.transform.rotation = Quaternion.Euler(0, objectRotation, 0);    
+    }
+}
