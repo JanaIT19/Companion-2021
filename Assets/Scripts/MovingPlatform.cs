a8c2d0964b4985764a071e84a909b978ffe58cdc
Jana <jana.pr.art@gmail.com>
2021-05-19 19:30:42 +0300

    Art and moving platform prefab
    Yay, coroutine

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float height;
+    [SerializeField]
+    private float pauseTime;
+    
+    private Vector3 copyOfPosition;
+
+    void Start()
+    {
+        StartCoroutine(MovePosition((transform.position + new Vector3(0, height, 0)), speed));
+    }
+
+    IEnumerator MovePosition(Vector3 destination, float speed)
+    {
+        copyOfPosition = transform.position;
+        while (transform.position != destination)
+        {
+            transform.position = Vector3.Lerp(transform.position, destination, speed * Time.deltaTime);
+            yield return null;
+        }
+        destination = copyOfPosition;
+        yield return new WaitForSeconds(pauseTime);
+        StartCoroutine(MovePosition(destination, speed));
+    }
+}
